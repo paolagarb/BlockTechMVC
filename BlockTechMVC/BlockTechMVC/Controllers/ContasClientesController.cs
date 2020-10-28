b21d9aef78ab15ce1a164cc8cc2c14863b3fcf22
Paola Garbato <paolagarbato@gmail.com>
2020-10-28 15:54:12 -0300

    Adicionando 'contas vinculadas'

--- BlockTechMVC/BlockTechMVC/Controllers/ContasClientesController.cs
+++ BlockTechMVC/BlockTechMVC/Controllers/ContasClientesController.cs
@@ -60,6 +60,8 @@ namespace BlockTechMVC.Controllers
         // GET: ContasClientes/Details/5
         public async Task<IActionResult> Details(int? id)
         {
+            ViewData["ContaVinculada"] = "Conta Vinculada";
+
             if (id == null)
             {
                 return NotFound();
