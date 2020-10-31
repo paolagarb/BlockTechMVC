96a4b0bc0f88360268a5c154dc6d8478add8fcf7
Paola Garbato <paolagarbato@gmail.com>
2020-10-30 22:18:32 -0300

    Implementando invetimentos dos usuários

---
+++ BlockTechMVC/BlockTechMVC/Controllers/MeusInvestimentosController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BlockTechMVC.Data;
+using BlockTechMVC.Models;
+
+namespace BlockTechMVC.Controllers
+{
+    public class MeusInvestimentosController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MeusInvestimentosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: MeusInvestimentos
+        public IActionResult Index()
+        {
+            var user = User.Identity.Name;
+
+            if (user == "Administrador")
+            {
+              
+            }
+            else
+            {
+                
+            }
+        }
+
+        
+        public double CriptomoedasCompras(string criptomoeda, string user)
+        {
+            var investimentos = (from transacoes in _context.Transacao
+                                 join conta in _context.ContaCliente
+                                 on transacoes.ContaClienteId equals conta.Id
+                                 join usuario in _context.ApplicationUser
+                                 on conta.ApplicationUserID equals usuario.Id
+                                 join criptomoedahoje in _context.CriptomoedaHoje
+                                 on transacoes.CriptomoedaHojeId equals criptomoedahoje.Id
+                                 join criptomoedas in _context.Criptomoeda
+                                 on criptomoedahoje.CriptomoedaId equals criptomoedas.Id
+                                 where criptomoedas.Nome == criptomoeda &&
+                                 transacoes.Tipo.Equals("Compra") &&
+                                 usuario.UserName == user
+                                 select transacoes.Valor).Single();
+            return investimentos;
+        }
+
+        public double CriptomoedasVendas(string criptomoeda, string user)
+        {
+            var investimentos = (from transacoes in _context.Transacao
+                                 join conta in _context.ContaCliente
+                                 on transacoes.ContaClienteId equals conta.Id
+                                 join usuario in _context.ApplicationUser
+                                 on conta.ApplicationUserID equals usuario.Id
+                                 join criptomoedahoje in _context.CriptomoedaHoje
+                                 on transacoes.CriptomoedaHojeId equals criptomoedahoje.Id
+                                 join criptomoedas in _context.Criptomoeda
+                                 on criptomoedahoje.CriptomoedaId equals criptomoedas.Id
+                                 where criptomoedas.Nome == criptomoeda &&
+                                 transacoes.Tipo.Equals("Venda") &&
+                                 usuario.UserName == user
+                                 select transacoes.Valor).Single();
+            return investimentos;
+        }
+        
+        public double CriptomoedasVendas(string criptomoeda, string user)
+        {
+            var investimentos = (from transacoes in _context.Transacao
+                                 join conta in _context.ContaCliente
+                                 on transacoes.ContaClienteId equals conta.Id
+                                 join usuario in _context.ApplicationUser
+                                 on conta.ApplicationUserID equals usuario.Id
+                                 join criptomoedahoje in _context.CriptomoedaHoje
+                                 on transacoes.CriptomoedaHojeId equals criptomoedahoje.Id
+                                 join criptomoedas in _context.Criptomoeda
+                                 on criptomoedahoje.CriptomoedaId equals criptomoedas.Id
+                                 where criptomoedas.Nome == criptomoeda &&
+                                 transacoes.Tipo.Equals("Transferencia") &&
+                                 usuario.UserName == user
+                                 select transacoes.Valor).Single();
+            return investimentos;
+        }
+
+        private bool TransacaoExists(int id)
+        {
+            return _context.Transacao.Any(e => e.Id == id);
+        }
+    }
+}
